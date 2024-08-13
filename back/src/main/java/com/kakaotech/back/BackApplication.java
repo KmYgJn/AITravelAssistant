1ad7fc3504b45b2a8987a8b886467d7abf87ffb8
Sin Han <vrdhan212@gmail.com>
2024-08-13 16:21:55 +0900

    test: 스프링 설정 정보 접근 테스트

--- back/src/main/java/com/kakaotech/back/BackApplication.java
+++ back/src/main/java/com/kakaotech/back/BackApplication.java
@@ -2,8 +2,11 @@ package com.kakaotech.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = DataSourceAutoConfiguration.class
+)
 public class BackApplication {
 
 	public static void main(String[] args) {
